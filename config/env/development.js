07286f4f6266d1051e8c28ef8bb4e807f23f261c
Vasandkumar <vasandkumar@Vasandkumars-MacBook-Pro.local>
2016-09-04 14:05:35 +0530

    Adding config values for db name and adding first call

--- config/env/development.js
+++ config/env/development.js
@@ -2,3 +2,4 @@
 module.exports.DB_HOST = 'DB_HOST';
 module.exports.DB_USERNAME = 'DB_USERNAME';
 module.exports.DB_PASSWORD = 'DB_PASSWORD';
+module.exports.DB_NAME = 'DB_NAME';
--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
+var mysqldb = require('./lib/mysqlhandler.js');
 
 app.get('/', function (req, res) {
-
-	
   res.json({hello:"this is a respone"});
+});
+
+app.get('/list', function (req, res){
+	var mysqlObject = new mysqldb();
+	mysqlObject.search("SELECT DISTINCT location, region FROM battles", function(data)
+		{
+			res.send(JSON.stringify(data));
+		});
+	
 });
 
 app.listen(process.env.port, function () {
